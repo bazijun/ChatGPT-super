5c187244c86246691f174995c8650421eee2c119
bazijun <1324022569@qq.com>
2023-03-24 17:16:20 +0800

    fix: 构建环境修改

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,12 @@ export default defineConfig((env) => {
   const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
 
   return {
+    base: './',
     resolve: {
       alias: {
         '@': path.resolve(process.cwd(), 'src'),
       },
     },
-    publicDir: './',
     plugins: [
       vue(),
       VitePWA({
