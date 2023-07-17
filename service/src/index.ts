0456ac186b40dc2c2dc9212976d8277a52f29c0b
bazijun <1324022569@qq.com>
2023-07-17 18:26:41 +0800

    fix: 服務器更新測試5

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import pkg from 'package.json'
+import moment from 'moment'
 import type { ChatContext, ChatMessage } from './chatgpt'
 import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
 import { auth } from './middleware/auth'
@@ -22,6 +23,10 @@ router.post('/chat-process', auth, async (req, res) => {
   const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL
   const defaultModel = isNotEmptyString(OPENAI_API_MODEL) ? OPENAI_API_MODEL : 'gpt-3.5-turbo'
   res.setHeader('Content-type', 'application/octet-stream')
+  const today = moment(new Date()).utcOffset(8)
+  global.console.log('this_time ===>', today.toLocaleString())
+  global.console.log('this_date ===>', today)
+  global.console.log('this_minute ===>', today.format('YYYY-MMdd'))
   try {
     const { prompt, systemMessage, model = defaultModel, options = {} } = req.body as { prompt: string; model: string; systemMessage: string; options?: ChatContext }
     let firstChunk = true
--- service/src/utils/index.ts
+++ service/src/utils/index.ts
@@ -25,10 +25,7 @@ export function sendResponse<T>(options: SendResponseOptions<T>) {
 
 /** 是否为某天 */
 export function isSomeDay(month: number, day: number) {
-  const today = moment().utcOffset(8).toDate()
-  global.console.log('this_time ===>', today.toLocaleString())
-  global.console.log('this_date ===>', today)
-  global.console.log('this_minute ===>', today.toLocaleTimeString())
+  const today = moment(new Date()).utcOffset(8).toDate()
   const Month = today.getMonth() + 1
   const Day = today.getDate()
   return (Month === month && Day === day)
