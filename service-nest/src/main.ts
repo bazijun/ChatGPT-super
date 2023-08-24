7156c72bd406de3052b7ad5bdbd9c57b36dd2a43
bazijun <1324022569@qq.com>
2023-08-24 11:06:10 +0800

    fix: docker配置4

--- service-nest/src/main.ts
+++ service-nest/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
-	await app.listen(3002, () => {
+	const configService = app.get(ConfigService);
+	const CONTAINER_PORT = configService.get('CONTAINER_PORT');
+	const HOST_PORT = configService.get('HOST_PORT');
+	await app.listen(CONTAINER_PORT, () => {
 		import('../package.json').then((res) => {
 			console.log(
-				`【⚡ CHATGPT_NEST_SERVICE：${res?.version} IS RUNNING ON PORT 3002 ⚡】`,
+				`【⚡ CHATGPT_NEST_SERVICE：${res?.version} IS RUNNING ON PORT ${HOST_PORT} ⚡】`,
 			);
 		});
 	});
