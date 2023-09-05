1df96612e50ee9b917e0146ef229065e4a3858cd
bazijun <1324022569@qq.com>
2023-09-05 18:16:05 +0800

    fix:应用即时更新

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig((env) => {
   // const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
-
   return {
     base: './',
     resolve: {
@@ -17,9 +16,15 @@ export default defineConfig((env) => {
       vue(),
       VitePWA({
         injectRegister: 'auto',
+        registerType: 'autoUpdate', // 开启自动更新
+        includeAssets: ['favicon.svg'], // 需要缓存的其他资源
+        workbox: {
+          skipWaiting: true, // 新版本立即接管网站
+          clientsClaim: true, // 新版本立即生效
+        },
         manifest: {
-          name: 'chatGPT',
-          short_name: 'chatGPT',
+          name: 'chatBPT',
+          short_name: 'chatBPT',
           icons: [
             { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
             { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
@@ -34,13 +39,6 @@ export default defineConfig((env) => {
       watch: {
         usePolling: true,
       },
-      // proxy: {
-      //   '/api': {
-      //     target: viteEnv.VITE_APP_API_BASE_URL,
-      //     changeOrigin: true, // 允许跨域
-      //     rewrite: path => path.replace('/api/', '/'),
-      //   },
-      // },
     },
     build: {
       reportCompressedSize: false,
