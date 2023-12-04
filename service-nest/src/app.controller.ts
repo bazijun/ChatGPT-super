45e87911be2933ae4c59b8b7a0c83cd564555dc5
bazijun <1324022569@qq.com>
2023-12-04 15:23:35 +0800

    feat: 添加佳宝密码

--- service-nest/src/app.controller.ts
+++ service-nest/src/app.controller.ts
@@ -4,19 +4,19 @@ import {
 	HttpException,
 	HttpStatus,
 	Post,
-} from "@nestjs/common";
-import { ChatgptService } from "./chatgpt/chatgpt.service";
+} from '@nestjs/common';
+import { ChatgptService } from './chatgpt/chatgpt.service';
 
 @Controller()
 export class AppController {
 	constructor(private readonly chatgptService: ChatgptService) {}
 
-	@Post("session")
+	@Post('session')
 	session() {
 		const hasAuth = true;
 		return {
-			status: "Success",
-			message: "",
+			status: 'Success',
+			message: '',
 			data: {
 				auth: hasAuth,
 				model: this.chatgptService.apiModel,
@@ -24,16 +24,16 @@ export class AppController {
 		};
 	}
 
-	@Post("verify")
+	@Post('verify')
 	verify(@Body() body: { token: string }) {
 		const { token } = body;
-		const TokenList = ["bzj", "psy", "nn", "zzl"];
+		const TokenList = ['bzj', 'jjb', 'psy', 'nn', 'zzl'];
 		if (!token)
-			throw new HttpException("授权码不能为空", HttpStatus.UNAUTHORIZED);
+			throw new HttpException('授权码不能为空', HttpStatus.UNAUTHORIZED);
 
 		if (!TokenList.includes(token))
-			throw new HttpException("授权码无效，请联系管理员", HttpStatus.FORBIDDEN);
+			throw new HttpException('授权码无效，请联系管理员', HttpStatus.FORBIDDEN);
 
-		return { status: "Success", message: "Verify successfully", data: null };
+		return { status: 'Success', message: 'Verify successfully', data: null };
 	}
 }
