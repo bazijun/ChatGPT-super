0c601acfefad431b23f7590498bb7e2129c3641b
bazijun <1324022569@qq.com>
2024-01-02 15:58:32 +0800

    fix: 服务大小写区分

--- service-nest/src/plugin/plugin.module.ts
+++ service-nest/src/plugin/plugin.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PluginController } from './Plugin.controller';
+import { PluginController } from './plugin.controller';
 
 @Module({
 	controllers: [PluginController],
